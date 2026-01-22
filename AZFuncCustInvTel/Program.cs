bf525e79f179beb3df25f438d0ec11a2fea76ecb
TCBWZA <tbw_@KHRONOS>
2026-01-22 15:09:47 +0000

    Change project to disable worker extension csproj

--- AZFuncCustInvTel/Program.cs
+++ AZFuncCustInvTel/Program.cs
@@ -4,35 +4,34 @@ using AZFuncCustInvTel.Repositories;
 using AZFuncCustInvTel.Validators;
 using FluentValidation;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        // Application Insights telemetry
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
 
-builder.ConfigureFunctionsWebApplication();
+        // Register Entity Framework Core DbContext
+        services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+        {
+            var configuration = serviceProvider.GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>();
+            options.UseSqlServer(configuration["DefaultConnection"]);
+        });
 
-// Application Insights telemetry
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+        // Register repositories
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+        services.AddScoped<ITelephoneNumberRepository, TelephoneNumberRepository>();
 
-// Register Entity Framework Core DbContext
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration["DefaultConnection"]));
+        // Register FluentValidation validators
+        services.AddScoped<IValidator<CreateCustomerDto>, CreateCustomerValidator>();
+        services.AddScoped<IValidator<UpdateCustomerDto>, UpdateCustomerValidator>();
+    })
+    .Build();
 
-// Register repositories
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
-builder.Services.AddScoped<ITelephoneNumberRepository, TelephoneNumberRepository>();
-
-// Register FluentValidation validators
-// These are used by CustomerFunctionsWithFluentValidation
-builder.Services.AddScoped<IValidator<CreateCustomerDto>, CreateCustomerValidator>();
-builder.Services.AddScoped<IValidator<UpdateCustomerDto>, UpdateCustomerValidator>();
-
-// Alternative: Register all validators in the assembly automatically
-// builder.Services.AddValidatorsFromAssemblyContaining<CreateCustomerValidator>();
-
-builder.Build().Run();
+host.Run();
